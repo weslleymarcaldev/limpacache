5a45a277db977654ce2e339ac0dd6da8651ee3d1
weslleymarcaldev <weslleymarcaldev@gmail.com>
2026-04-14 18:12:39 -0300

    fix: update Content Security Policy to allow unsafe-inline styles and add extension recommendations

--- src/webviewPanel.ts
+++ src/webviewPanel.ts
@@ -190,7 +190,8 @@ export class WebviewPanel {
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src 'nonce-${nonce}'; script-src 'nonce-${nonce}';">
+<meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src 'nonce-${nonce}' 'unsafe-inline'; script-src 'nonce-${nonce}';">
+
 <title>LimpaCache</title>
 <style nonce="${nonce}">
 :root {
